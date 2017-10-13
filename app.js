0455017691be6a5d837e2dcd28c9908283d30273
Bright Almighty <muhumuzabri@gmail.com>
2017-10-13 10:59:35 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var app = angular.module('app',
+ [
+     'ngResource',
+     'gitHubService'
+
+ ]
+);
+
+//specify dependencies for our angular spp namely : ngResource library
+// and our factory githubService
---
+++ app/app.js
@@ -0,0 +1,9 @@
+var app = angular.module('app',
+    [
+        'ngResource',
+        'gitHubService'
+    ]
+);
+
+//specify dependencies for our angular spp namely : ngResource library
+// and our factory githubService
